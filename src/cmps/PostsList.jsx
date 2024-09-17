216ca3a5c47737244a41f9fd441d25d4a6873dbb
Roman Puchinsky <romanpu@gmail.com>
2024-09-17 15:55:19 +0300

    added list of posts

--- src/cmps/PostsList.jsx
+++ src/cmps/PostsList.jsx
@@ -6,8 +6,6 @@ import { PostAddSharp } from '@mui/icons-material'
 
 export function PostsList() {
     const posts = useSelector(storeState => storeState.postsModule.posts)
-    console.log(posts)
-    if (posts.length) return <PostPreview user = {posts[0].author} date ={posts[0].createdAt} content = {posts[0].body}/>
-    return <div>loading</div>
+    return posts.map((post) =><PostPreview user = {post.author} date ={post.createdAt} content = {post.body}/>)
 }
 
