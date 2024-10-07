7df23c900064a3222c7bc4597537cbefad2b843f
Roman Puchinsky <romanpu@gmail.com>
2024-10-07 17:06:34 +0300

    fixed reload on explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { PostGallery } from '../cmps/PostGallery';
 import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router';
+import { useEffect } from 'react'
+import { LoadPosts } from '../store/posts/posts.actions'
 
 export function Explore(){
     const posts = useSelector(storeState => storeState.postsModule.posts)
     console.log("gallery", posts)
+
+    useEffect(() => {
+        LoadPosts()
+    }, [])
 
     return <div className="explore-page">
         <PostGallery posts = {posts}/>
