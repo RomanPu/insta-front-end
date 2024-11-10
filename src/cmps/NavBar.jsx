7ba5cdc393ee6a8e08047d7ae6a08742b6741c18
Roman Puchinsky <romanpu@gmail.com>
2024-11-10 12:13:55 +0200

    added more styling to nav bar and changed logo to instush

--- src/cmps/NavBar.jsx
+++ src/cmps/NavBar.jsx
@@ -4,7 +4,7 @@ import { HomeIcon, SearchIcon, ExploreIcon, ReelsIcon, MessengerIcon,
 NotificationsIcon, NewPostIcon, SettingsIcon } from '../assets/imgs/NavBarSvgs';
 import { Avatar } from './Avatar';
 import { useSelector } from 'react-redux';
-
+import poster from '../assets/imgs/Sticker.png';
 
 import { InstagramLogo } from '../assets/imgs/LogoSvgs';
 
@@ -12,7 +12,7 @@ export function NavBar() {
   const logedUser = useSelector(storeState => storeState.logedUserModule.logedUser)
 
   return <ul className="nav-bar" > 
-            <div key={"ins-logo"} className='insta-logo'><InstagramLogo/></div>
+            <div key={"ins-logo"} className='insta-logo'><img src={poster} alt="Instagram Logo" /></div>
             <li key={"home"}><NavBarAction name={"Home"} link={"/"} icon={<HomeIcon />} /></li>
             <li key={"search"}><NavBarAction name={"Search"} icon={<SearchIcon />} /></li>
             <li key={"explore"}><NavBarAction name={"Explore"}  icon={<ExploreIcon/>}  link={"/explore"}/></li>
@@ -20,7 +20,8 @@ export function NavBar() {
             <li key={"messeges"}><NavBarAction name={"Messeges"}  icon={<MessengerIcon />} /></li>
             <li key={"notifications"}><NavBarAction name={"Notifications"}  icon={<NotificationsIcon />} /></li>
             <li key={"create"}><NavBarAction name={"Create"}  icon={<NewPostIcon />} link={"/createpost"} /></li>
-            <li key={"profile"}><NavBarAction name={"Profile"} link={`/profile/${logedUser._id}`} icon={<Avatar/>} /></li>
+            <li key={"profile"}><NavBarAction name={"Profile"} link={`/profile/${logedUser._id}`} icon={<Avatar 
+            picUrl={logedUser.avatarPic} />} /></li>
             <li key={"more"}><NavBarAction name={"More"}  icon={<SettingsIcon />} /></li>
   </ul>  
 }
