084e736eed5b6740e8763a175f1773cd370b8b4d
Roman Puchinsky <romanpu@gmail.com>
2024-11-21 12:33:12 +0200

    imp edit profile page

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
 import { Avatar } from '../cmps/Avatar';
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import logo from '../assets/imgs/Sticker.png'
+import { editUser } from '../store/users/users.actions';
+import { useNavigate } from 'react-router-dom';
+import { uploadService } from '../services/img.upload.service'
 
 
 export function EditProfile() {
     const [count, setCount] = useState(0)
-    const [bio, setBio] = useState('')
-    const loggedUser = useSelector(storeState => storeState.logedUserModule.logedUser);
+    const {_id}= useSelector(storeState => storeState.logedUserModule.logedUser);
+    const loggedUser = useSelector(storeState => storeState.usersModule.users.find(user => user._id === _id));
+    const [gImgUrl, setGImgUrl] = useState(loggedUser.avatarPic)
+    const [bio, setBio] = useState(loggedUser.body)
+    const navigate = useNavigate()
+    const fileInputRef = useRef(null)
+
+    function onSelectImg() {
+        if (fileInputRef.current) {
+            fileInputRef.current.click()
+            console.log('clicked')
+        }
+    }
+
+    async function onFileChange(event) {
+        const file = event.target.files[0]
+        if (file) {
+            let imgUrl = await uploadService.uploadImg(file)
+            setGImgUrl(imgUrl.secure_url)
+            console.log(imgUrl)
+            // getImgUrl(imgUrl)
+            // setState('pic-post')
+        }
+    }
 
     function handleCommentChange(event) {
         setBio(event.target.value)
         setCount(event.target.value.length)
+    }
+
+    function onSubmit() {
+        // console.log(bio)
+        editUser({...loggedUser , body: bio, avatarPic: gImgUrl})
+        navigate(-1)
     }
 
     return (
@@ -20,13 +51,22 @@ export function EditProfile() {
                 <h1>Edit Profile</h1>
                 <div className = 'user-card-conteiner'>
                     <div className="user-card" >
-                        <Avatar picUrl={loggedUser.avatarPic} />
+                        <Avatar picUrl={gImgUrl} />
                         <div className="user-info">
                             <h1>{loggedUser.username}</h1>
                             <p>{loggedUser.username}</p>
                         </div>
-                        <button>Change photo</button>       
+                        <button onClick={onSelectImg}>Change photo</button>
+                        <input
+                            type="file"
+                            ref={fileInputRef}
+                            style={{ display: 'none' }}
+                            onChange={onFileChange}
+                            accept="img/*"
+                            id="imgUpload"
+                        />       
                     </div> 
+                    <h3>Bio</h3>
                     <div className="write-bio">
                         <textarea
                             // ref={textareaRef}
@@ -39,7 +79,15 @@ export function EditProfile() {
                         <div className="emoji-and-count">
                             <span>{count}/150</span>
                         </div>
-                    </div>  
+                    </div> 
+                    <h3>Gender</h3>
+                    <select name="gender-select">
+                            <option>Male</option>
+                            <option>Female</option>
+                            <option>Other</option>
+                    </select> 
+                    <button className='submit' onClick={onSubmit}>Submit</button>
+                    <img className = {logo} src={logo} alt="Instagram Logo" />
                 </div>
             </div>
         </div>
