c348473d7643bc6006c10381a707d6e3cf45911d
Roman Puchinsky <romanpu@gmail.com>
2024-11-25 20:06:12 +0200

    style improvment for notification

--- src/cmps/Notification.jsx
+++ src/cmps/Notification.jsx
@@ -1,6 +1,10 @@
 import { Avatar } from './Avatar'
 import { Link } from 'react-router-dom'
 import { utilService } from '../services/util.service'
+import { useState } from 'react'
+import { useEffectUpdate } from '../customHooks/useEffectUpdate'
+import { useSelector } from 'react-redux'
+import { editUser } from '../store/users/users.actions'
 
 
 
@@ -12,6 +16,35 @@ export function Notification( {about,  body, createdAt, post, byUser} ) {
 
 function Deteils({byUser,about,  body, createdAt, post}) {
     const timeStr =utilService.createPostTimeFormat(createdAt)
+    const { _id } = useSelector(storeState => storeState.logedUserModule.logedUser)
+    const loggedUser = useSelector(storeState => storeState.usersModule.users.find(user => user._id === _id))
+    const author = useSelector(storeState => storeState.usersModule.users.find(u => u._id === byUser._id))
+    const [isFollowed, setIsFollowed] = useState(
+        loggedUser.following.map(follow => follow === author._id).includes(true)
+    )
+
+    useEffectUpdate(() => {
+        if (isFollowed && !loggedUser.following.map(like => like === byUser._id).includes(true)) {
+            onFolow()
+        } else {
+            editUser({
+                ...loggedUser,
+                following: loggedUser.following.filter(follow => follow !== author._id)
+            })
+        }
+    }, [isFollowed])
+
+    async function onFolow() {
+        await editUser({ ...author, followers: [...author.followers, _id] })
+        await editUser({ ...loggedUser, following: [...loggedUser.following, author._id] }, 'follow', author._id)
+    }
+
+    function onChangeFollow(ev) {
+        console.log('ev', ev)
+        ev.stopPropagation()
+        setIsFollowed(prev => !prev)
+    }
+    
     return (
         <li className = {"notification-entry"}key={byUser._id}>
             <div className="notification-deteils">
@@ -28,6 +61,16 @@ function Deteils({byUser,about,  body, createdAt, post}) {
                 </div>
             </div>
             {post && <Link to = {`../post/${post.postId}`}><img src = {post.picUrl} /></Link>}
+            {!post && isFollowed && (
+                <button onClick={(ev) => onChangeFollow(ev)} className="followed">
+                    Followed
+                </button>
+            )}
+            {!post && !isFollowed &&  (
+                <button onClick={(ev) => onChangeFollow(ev)} className="follow">
+                    Follow
+                </button>
+            )}
         </li>
     )
 }
--- src/cmps/NotificationPopUp.jsx
+++ src/cmps/NotificationPopUp.jsx
@@ -1,12 +1,12 @@
-
-import { useEffect, useState} from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { Notification } from './Notification'
 import { useSelector } from 'react-redux'
 import { readNotifications } from '../store/logedUser/loged.user.actions'
 
-export function NotificationPopUp({onClose}) {
-    const [show, setShow] = useState("")// for slide effect
+export function NotificationPopUp({ onClose }) {
+    const [show, setShow] = useState("") // for slide effect
     const notifications = useSelector(storeState => storeState.logedUserModule.notifications)
+    const popupRef = useRef(null)
 
     useEffect(() => {
         setShow("show")
@@ -18,22 +18,30 @@ export function NotificationPopUp({onClose}) {
         }
     }, [])
 
-    function handleClickOutside() {
-        setTimeout(() => {
-            onClose(false)
-            setShow("hide")
-        }, 300)
-
+    function handleClickOutside(event) {
+        if (popupRef.current && !popupRef.current.contains(event.target)) {
+            setTimeout(() => {
+                onClose(false)
+                setShow("hide")
+            }, 300)
+        }
     }
 
     return (
-            <div className={`notification-pop-up ${show}`}>
-                <h3>Notifications</h3>
-                <ul className="notification-pop-up-body">
-                    {notifications.map(notification => { return <Notification byUser = {notification.byUser} 
-                    about = {notification.about} body = {notification.body}
-                   createdAt={+notification.createdAt} post = {notification.post}/>})}
-                </ul>
-            </div>
+        <div ref={popupRef} className={`notification-pop-up ${show}`}>
+            <h3>Notifications</h3>
+            <ul className="notification-pop-up-body">
+                {notifications.map(notification => (
+                    <Notification
+                        key={notification._id}
+                        byUser={notification.byUser}
+                        about={notification.about}
+                        body={notification.body}
+                        createdAt={+notification.createdAt}
+                        post={notification.post}
+                    />
+                ))}
+            </ul>
+        </div>
     )
 }
